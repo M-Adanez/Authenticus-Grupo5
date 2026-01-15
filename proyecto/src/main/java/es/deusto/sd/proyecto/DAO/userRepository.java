041b0eb8ddb3937e5a90a8e28c8df76d9936b1e9
Iñigo Zubiaguirre <inigo.zubiaguirre@opendeusto.es>
2026-01-15 12:38:34 +0100

    hbjhj

--- proyecto/src/main/java/es/deusto/sd/proyecto/DAO/userRepository.java
+++ proyecto/src/main/java/es/deusto/sd/proyecto/DAO/userRepository.java
@@ -3,11 +3,13 @@ package es.deusto.sd.proyecto.DAO;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import es.deusto.sd.proyecto.Entity.User;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface userRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
     
     // Añade estos dos métodos:
     boolean existsByUsername(String username);
+    @Transactional
     void deleteByUsername(String username);
 }
